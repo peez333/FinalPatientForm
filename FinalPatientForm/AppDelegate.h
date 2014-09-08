d48b9d660946a8bc2aafb619c0f11bb7a0bd9150
Peeyush <peeyush@INHYKPMAC01-MacBook-Pro.local>
2014-09-08 12:23:15 +0530

    Initial Commit

---
+++ FinalPatientForm/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FinalPatientForm
+//
+//  Created by Peeyush on 08/09/14.
+//  Copyright (c) 2014 sutherland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ FinalPatientForm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FinalPatientForm
+//
+//  Created by Peeyush on 08/09/14.
+//  Copyright (c) 2014 sutherland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
